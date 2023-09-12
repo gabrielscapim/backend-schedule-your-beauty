f576a0aab707398fbf33c3bdb424c822a038a893
Gabriel <scapimgabriel@gmail.com>
2023-09-12 19:07:39 -0300

    feat: production model

---
+++ api/src/main/java/schedule/your/beauty/api/model/Production.java
@@ -0,0 +1,28 @@
+package schedule.your.beauty.api.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "productions")
+@Entity(name = "production")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Production {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name", length = 45, nullable = false)
+    private String name;
+
+    @Column(name = "value", nullable = false)
+    @Min(0)
+    private int value;
+}
