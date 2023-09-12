ad86f082f6b1d31de70e6d5fd478aa4b98e3b2d6
Gabriel <scapimgabriel@gmail.com>
2023-09-12 19:12:28 -0300

    feat: schedule time model

---
+++ api/src/main/java/schedule/your/beauty/api/model/ScheduleTime.java
@@ -0,0 +1,31 @@
+package schedule.your.beauty.api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Table(name = "scheduling_times")
+@Entity(name = "schedule_time")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class ScheduleTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "date_time")
+    private Date dateTime;
+
+    @Column(name = "last_schedule_time_day")
+    private boolean lastScheduleTimeDay;
+
+    @Column(name = "available")
+    private boolean available;
+}
