25086494391cd15080d8c0c0a9c936dc91b35fe7
Gabriel <scapimgabriel@gmail.com>
2023-09-19 14:54:48 -0300

    fix: get scheduling date time endpoint

--- api/src/main/java/schedule/your/beauty/api/repository/SchedulingDateTimeRepository.java
+++ api/src/main/java/schedule/your/beauty/api/repository/SchedulingDateTimeRepository.java
@@ -11,25 +11,33 @@ public interface SchedulingDateTimeRepository extends JpaRepository<SchedulingDa
   @Query(value = "SELECT DATE_FORMAT(date_time, '%H:%i')\n" +
     "FROM scheduling_times t1\n" +
     "WHERE \n" +
-    "    DATE(t1.date_time) = :dateTime\n" +
+    "    DATE(t1.date_time) = '2023-09-19'\n" +
     "    AND (\n" +
     "        (t1.last_schedule_time_day = FALSE \n" +
     "            AND EXISTS (\n" +
     "                SELECT 1\n" +
     "                FROM scheduling_times t2\n" +
     "                WHERE t2.date_time = t1.date_time + INTERVAL 30 MINUTE\n" +
+    "                AND t2.available = TRUE\n" +
     "            ) \n" +
     "            AND EXISTS (\n" +
     "                SELECT 1\n" +
     "                FROM scheduling_times t3\n" +
     "                WHERE t3.date_time = t1.date_time + INTERVAL 60 MINUTE\n" +
+    "                AND t3.available = TRUE\n" +
     "            )\n" +
     "        )\n" +
-    "        OR\n" +
-    "        t1.last_schedule_time_day = TRUE\n" +
+    "        OR t1.last_schedule_time_day = TRUE\n" +
+    "        OR EXISTS (\n" +
+    "            SELECT 1\n" +
+    "            FROM scheduling_times t2\n" +
+    "            WHERE t2.date_time = t1.date_time + INTERVAL 30 MINUTE\n" +
+    "            AND last_schedule_time_day = TRUE\n" +
+    "            AND t2.available = TRUE\n" +
+    "        )\n" +
     "    )\n" +
     "    AND t1.available = TRUE\n" +
-    "    ORDER BY t1.date_time;", nativeQuery = true)
+    "ORDER BY t1.date_time;", nativeQuery = true)
   List<String> findAvailableSchedulingTimesByDayForMake(@Param("dateTime") String dateTime);
 
   @Query(value = "SELECT DATE_FORMAT(date_time, '%H:%i')\n" +
@@ -42,23 +50,39 @@ public interface SchedulingDateTimeRepository extends JpaRepository<SchedulingDa
     "                SELECT 1\n" +
     "                FROM scheduling_times t2\n" +
     "                WHERE t2.date_time = t1.date_time + INTERVAL 30 MINUTE\n" +
+    "                AND t2.available = TRUE\n" +
     "            ) \n" +
     "            AND EXISTS (\n" +
     "                SELECT 1\n" +
     "                FROM scheduling_times t3\n" +
     "                WHERE t3.date_time = t1.date_time + INTERVAL 60 MINUTE\n" +
+    "                AND t3.available = TRUE\n" +
     "            )\n" +
     "            AND EXISTS (\n" +
     "                SELECT 1\n" +
     "                FROM scheduling_times t4\n" +
     "                WHERE t4.date_time = t1.date_time + INTERVAL 90 MINUTE\n" +
+    "                AND t4.available = TRUE\n" +
     "            )\n" +
     "        )\n" +
-    "        OR\n" +
-    "        t1.last_schedule_time_day = TRUE\n" +
+    "        OR t1.last_schedule_time_day = TRUE\n" +
+    "        OR EXISTS (\n" +
+    "            SELECT 1\n" +
+    "            FROM scheduling_times t2\n" +
+    "            WHERE t2.date_time = t1.date_time + INTERVAL 30 MINUTE\n" +
+    "            AND last_schedule_time_day = TRUE\n" +
+    "            AND t2.available = TRUE\n" +
+    "        )\n" +
+    "        OR EXISTS (\n" +
+    "            SELECT 1\n" +
+    "            FROM scheduling_times t3\n" +
+    "            WHERE t3.date_time = t1.date_time + INTERVAL 60 MINUTE\n" +
+    "            AND last_schedule_time_day = TRUE\n" +
+    "            AND t3.available = TRUE\n" +
+    "        )\n" +
     "    )\n" +
     "    AND t1.available = TRUE\n" +
-    "    ORDER BY t1.date_time;", nativeQuery = true)
+    "ORDER BY t1.date_time;", nativeQuery = true)
   List<String> findAvailableSchedulingTimesByDayForMakeHair(@Param("dateTime") String dateTime);
 
   @Query(value = "SELECT DATE_FORMAT(date_time, '%H:%i')\n" +
@@ -69,8 +93,9 @@ public interface SchedulingDateTimeRepository extends JpaRepository<SchedulingDa
     "    ORDER BY t1.date_time;\n", nativeQuery = true)
   List<String> findAvailableSchedulingTimesByDayForHair(@Param("dateTime") String dateTime);
 
-  @Query(value = "SELECT DISTINCT DATE(date_time)\n" +
-    "FROM scheduling_times\n" +
-    "ORDER BY DATE(date_time);", nativeQuery = true)
+  @Query(value = "SELECT DISTINCT DATE(date_time) \n" +
+          "FROM scheduling_times\n" +
+          "WHERE available = true\n" +
+          "ORDER BY DATE(date_time)", nativeQuery = true)
   List<String> findSchedulingDates();
 }
