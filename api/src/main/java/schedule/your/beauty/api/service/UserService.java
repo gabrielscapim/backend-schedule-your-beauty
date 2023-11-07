815a41cbb41a20b24770970c31744d06b6f91e0c
Gabriel <scapimgabriel@gmail.com>
2023-11-07 09:48:43 -0300

    feat: user service

---
+++ api/src/main/java/schedule/your/beauty/api/service/UserService.java
@@ -0,0 +1,26 @@
+package schedule.your.beauty.api.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import schedule.your.beauty.api.model.User;
+import schedule.your.beauty.api.repository.UserRepository;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username);
+    }
+}
