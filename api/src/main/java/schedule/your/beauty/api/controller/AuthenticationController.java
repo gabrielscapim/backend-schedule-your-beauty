689a77dcf2b53ccadbd0cf84d38b715ef82327c6
Gabriel <scapimgabriel@gmail.com>
2023-11-08 08:47:49 -0300

    feat: authentication controller

---
+++ api/src/main/java/schedule/your/beauty/api/controller/AuthenticationController.java
@@ -0,0 +1,50 @@
+package schedule.your.beauty.api.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import schedule.your.beauty.api.dto.TokenDTO;
+import schedule.your.beauty.api.dto.UserDTO;
+import schedule.your.beauty.api.model.User;
+import schedule.your.beauty.api.service.TokenService;
+import schedule.your.beauty.api.service.UserService;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserService userService;
+    private final TokenService tokenService;
+
+    @Autowired
+    public AuthenticationController(
+            AuthenticationManager authenticationManager,
+            UserService userService,
+            TokenService tokenService
+    ) {
+        this.authenticationManager = authenticationManager;
+        this.userService = userService;
+        this.tokenService = tokenService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenDTO> login(@RequestBody @Valid UserDTO userDTO) {
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                new UsernamePasswordAuthenticationToken(userDTO.username(), userDTO.password());
+        Authentication authentication = authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+
+        User user = (User) authentication.getPrincipal();
+        String token = tokenService.generateToken(user);
+        TokenDTO tokenDTO = new TokenDTO(token);
+
+        return ResponseEntity.ok().body(tokenDTO);
+    }
+}
