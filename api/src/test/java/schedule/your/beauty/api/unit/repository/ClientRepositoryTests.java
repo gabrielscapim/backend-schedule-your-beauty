d96b1c3092556cdd1fcad9f27bdbf9f723cfca05
Gabriel <scapimgabriel@gmail.com>
2023-11-08 15:09:20 -0300

    test: client repository

---
+++ api/src/test/java/schedule/your/beauty/api/unit/repository/ClientRepositoryTests.java
@@ -0,0 +1,41 @@
+package schedule.your.beauty.api.unit.repository;
+
+
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import schedule.your.beauty.api.model.Client;
+import schedule.your.beauty.api.repository.ClientRepository;
+import static org.assertj.core.api.Assertions.*;
+
+@DataJpaTest
+@ActiveProfiles("test")
+public class ClientRepositoryTests {
+
+  @Autowired
+  ClientRepository clientRepository;
+  @Autowired
+  EntityManager entityManager;
+
+  @Test
+  @DisplayName("Should get Client sucessfully from DB")
+  void findClientByNumberSuccess() {
+    String clientNumber = "99999999";
+    this.addClient("João", clientNumber);
+    Client foundedClient = this.clientRepository.findClientByNumber(clientNumber);
+
+    assertThat(foundedClient.getNumber()).isEqualTo(clientNumber);
+  }
+
+  private Client addClient(String name, String number) {
+    Client newClient = new Client(name, number);
+    this.entityManager.persist(newClient);
+
+    return newClient;
+  }
+}
